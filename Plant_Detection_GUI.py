8a95a867e8daf5fced856af37006fb32f2e5514b
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2016-05-24 20:34:25 -0700

    Create Plant_Detection_GUI.py

---
+++ Plant_Detection_GUI.py
@@ -0,0 +1,26 @@
+import cv2
+from soil_image_plant_detection import detect_plants
+
+filename = 'soil_image.jpg'
+
+def process(x):
+    img = detect_plants(filename, 
+          blur=blur, morph=morph, iterations=iterations, 
+          debug=True, save=False)
+    cv2.imshow('image', img)
+
+cv2.namedWindow('image')
+cv2.createTrackbar('Blur', 'image', 0, 100, process)
+cv2.createTrackbar('Morph', 'image', 1, 100, process)
+cv2.createTrackbar('Iterations', 'image', 1, 100, process)
+
+while(1):
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27: break
+
+    blur = cv2.getTrackbarPos('Blur', 'image')
+    if blur % 2 == 0: blur += 1
+    morph = cv2.getTrackbarPos('Morph', 'image')
+    iterations = cv2.getTrackbarPos('Iterations', 'image')
+
+cv2.destroyAllWindows()
