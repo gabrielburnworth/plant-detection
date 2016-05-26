465369d3480125c7a6e97e37e594452b5f272bf7
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2016-05-26 09:51:34 -0700

    Improved window name

--- Plant_Detection_GUI.py
+++ Plant_Detection_GUI.py
@@ -2,13 +2,14 @@ import cv2
 from soil_image_plant_detection import detect_plants
 
 filename = 'soil_image.jpg'
+window = 'Plant Detection'
 
 def process(x):
     # Get parameter values
-    blur = cv2.getTrackbarPos('Blur', 'image')
+    blur = cv2.getTrackbarPos('Blur', window)
     if blur % 2 == 0: blur += 1
-    morph = cv2.getTrackbarPos('Morph', 'image')
-    iterations = cv2.getTrackbarPos('Iterations', 'image')
+    morph = cv2.getTrackbarPos('Morph', window)
+    iterations = cv2.getTrackbarPos('Iterations', window)
 
     # Process image with parameters
     img = detect_plants(filename, 
@@ -16,12 +17,12 @@ def process(x):
           debug=True, save=False)
 
     #Show processed image
-    cv2.imshow('image', img)
+    cv2.imshow(window, img)
 
-cv2.namedWindow('image')
-cv2.createTrackbar('Blur', 'image', 0, 100, process)
-cv2.createTrackbar('Morph', 'image', 1, 100, process)
-cv2.createTrackbar('Iterations', 'image', 1, 100, process)
+cv2.namedWindow(window)
+cv2.createTrackbar('Blur', window, 0, 100, process)
+cv2.createTrackbar('Morph', window, 1, 100, process)
+cv2.createTrackbar('Iterations', window, 1, 100, process)
 
 while(1):
     k = cv2.waitKey(1) & 0xFF
