74fca4674e4f69cc527da16385b43064985dd0f3
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2016-05-25 18:24:40 -0700

    Action order fix

--- Plant_Detection_GUI.py
+++ Plant_Detection_GUI.py
@@ -4,9 +4,18 @@ from soil_image_plant_detection import detect_plants
 filename = 'soil_image.jpg'
 
 def process(x):
+    # Get parameter values
+    blur = cv2.getTrackbarPos('Blur', 'image')
+    if blur % 2 == 0: blur += 1
+    morph = cv2.getTrackbarPos('Morph', 'image')
+    iterations = cv2.getTrackbarPos('Iterations', 'image')
+
+    # Process image with parameters
     img = detect_plants(filename, 
           blur=blur, morph=morph, iterations=iterations, 
           debug=True, save=False)
+
+    #Show processed image
     cv2.imshow('image', img)
 
 cv2.namedWindow('image')
@@ -17,10 +26,5 @@ cv2.createTrackbar('Iterations', 'image', 1, 100, process)
 while(1):
     k = cv2.waitKey(1) & 0xFF
     if k == 27: break
-
-    blur = cv2.getTrackbarPos('Blur', 'image')
-    if blur % 2 == 0: blur += 1
-    morph = cv2.getTrackbarPos('Morph', 'image')
-    iterations = cv2.getTrackbarPos('Iterations', 'image')
 
 cv2.destroyAllWindows()
