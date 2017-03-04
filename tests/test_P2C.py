71731fee35f146408fd6b81166709fe470d669e4
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2017-03-04 02:57:13 -0800

    more calibration tests

--- tests/test_P2C.py
+++ tests/test_P2C.py
@@ -20,13 +20,24 @@ class P2CTest(unittest.TestCase):
         self.two_objects = cv2.imread('PD/p2c_test_calibration.jpg', 1)
         self.three_objects = self.two_objects.copy()
         self.one_object = self.two_objects.copy()
+        self.zero_objects = self.two_objects.copy()
+        cv2.circle(self.zero_objects,
+                   (600, 300), int(1000),
+                   (255, 255, 255), -1)
         cv2.circle(self.one_object,
                    (175, 475), int(50),
                    (255, 255, 255), -1)
         cv2.circle(self.three_objects,
                    (600, 300), int(25),
                    (0, 0, 255), -1)
-        cv2.imwrite('image.jpg', self.three_objects)
+
+    def test_zero_objects(self):
+        """Detect zero objects during calibration"""
+        db = DB()
+        p2c = Pixel2coord(db, calibration_image=self.zero_objects)
+        exit_flag = p2c.calibration()
+        self.assertEqual(db.object_count, 0)
+        self.assertTrue(exit_flag)
 
     def test_one_object(self):
         """Detect one object during calibration"""
@@ -34,6 +45,7 @@ class P2CTest(unittest.TestCase):
         p2c = Pixel2coord(db, calibration_image=self.one_object)
         exit_flag = p2c.calibration()
         self.assertEqual(db.object_count, 1)
+        self.assertTrue(exit_flag)
 
     def test_two_objects(self):
         """Detect two objects during calibration"""
@@ -41,6 +53,7 @@ class P2CTest(unittest.TestCase):
         p2c = Pixel2coord(db, calibration_image=self.two_objects)
         exit_flag = p2c.calibration()
         self.assertEqual(db.object_count, 2)
+        self.assertFalse(exit_flag)
 
     def test_three_objects(self):
         """Detect three objects during calibration"""
@@ -48,6 +61,7 @@ class P2CTest(unittest.TestCase):
         p2c = Pixel2coord(db, calibration_image=self.three_objects)
         exit_flag = p2c.calibration()
         self.assertEqual(db.object_count, 3)
+        self.assertFalse(exit_flag)
 
     def tearDown(self):
         self.outfile.close()
